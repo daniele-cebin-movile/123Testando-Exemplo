3aa84452a3399f6b1f2ca877b80f7e78335b6c0b
Marcos Trovilho <marcos@trovilho.com>
2015-10-21 01:44:28 -0200

    adiciona classe contador no projeto
    testes ok

---
+++ Contador/Contador.swift
@@ -0,0 +1,13 @@
+//
+//  Contador.swift
+//  Contador
+//
+//  Created by Marcos Trovilho on 10/21/15.
+//  Copyright © 2015 Quaddro Treinamentos. All rights reserved.
+//
+
+import Foundation
+
+class Contador {
+    
+}
